0184cb1ab9ec6d521cdd6d5001c46e2c205cd00f
Caspari <caspari@students.uni-marburg.de>
2012-12-16 22:31:12 +0100

    in diesen Ordner verschoben, da sonst Eclipse rumgemeckert hat.

---
+++ src/de/unimarburg/eise12/servlet/Tester.java
@@ -0,0 +1,23 @@
+package de.unimarburg.eise12.servlet;
+
+
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Server server = setupServer();
+		server.get("/solution?name1=Max&surname1=Mustermann&name2=Luise&surname2=Musterfrau");
+		server.get("/composite?name1=Heinz&surname1=Schneider&name2=Petra&surname2=Meyer");
+	}
+	
+	static Server setupServer() {
+		Server server = new Server();
+
+		server.register("/solution", new CompatiblePartnerServlet());
+		server.register("/composite", new CompositeServlet(new CompatiblePartnerServlet(), new CompatiblePartnerServlet()));
+		return server;
+	}
+
+}
